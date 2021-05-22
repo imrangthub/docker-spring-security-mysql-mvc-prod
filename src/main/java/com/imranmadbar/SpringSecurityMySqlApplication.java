a618a3f04a51366f97e3cc6a2a3ffdbffdb44470
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2021-05-22 07:08:03 +0600

    Init Project

---
+++ src/main/java/com/imranmadbar/SpringSecurityMySqlApplication.java
@@ -0,0 +1,17 @@
+package com.imranmadbar;
+
+import java.io.IOException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestClientException;
+
+@SpringBootApplication
+public class SpringSecurityMySqlApplication {
+
+	public static void main(String[] args) throws RestClientException, IOException {
+		SpringApplication.run(SpringSecurityMySqlApplication.class, args);
+		System.out.println("Spring Security MySQL MVC Application Run Successfully Done !");
+
+	}
+
+}
