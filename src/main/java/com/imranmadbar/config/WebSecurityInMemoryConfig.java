cf3ac240677b1f3bcffaa1cccc0f95facb238013
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2021-05-28 11:39:31 +0600

    Upadate secirity with dao user details auth

--- src/main/java/com/imranmadbar/config/WebSecurityInMemoryConfig.java
+++ src/main/java/com/imranmadbar/config/WebSecurityInMemoryConfig.java
@@ -3,22 +3,16 @@ package com.imranmadbar.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import com.imranmadbar.config.CustomAccessDeniedHandler;
-import com.imranmadbar.config.CustomAuthenticationFailureHandler;
-import com.imranmadbar.config.CustomAuthenticationProvider;
-import com.imranmadbar.config.CustomAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -36,22 +30,31 @@ public class WebSecurityInMemoryConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
 	
-	
-//	@Override
-//	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication()
-//	        .withUser("imranmadbar").password(passwordEncoder().encode("imranmadbar")).roles("SUPERADMIN", "ADMIN", "MANAGER");
-//	}
-	
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(customAuthenticationProvider);
-		auth.eraseCredentials(false);
-	}
-	
-	
-	
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	auth.authenticationProvider(customAuthenticationProvider);
+    	auth.authenticationProvider(authProvider());
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        // By default super adming setup
+        UserDetails theUser = User.withUsername("imranmadbar")
+                        .password("imranmadbar").roles("SUPERADMIN").build();
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        userDetailsManager.createUser(theUser);
+        return userDetailsManager;
+    }
+
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
