4e5a8f4f0cd027e12376e3f3bfa84031bd0e4a72
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2021-05-29 11:07:41 +0600

    Upate app

--- src/main/java/com/imranmadbar/SpringSecurityMySqlApplication.java
+++ src/main/java/com/imranmadbar/SpringSecurityMySqlApplication.java
@@ -11,7 +11,16 @@ public class SpringSecurityMySqlApplication {
 
 	public static void main(String[] args) throws RestClientException, IOException {
 		SpringApplication.run(SpringSecurityMySqlApplication.class, args);
-		System.out.println("Spring Security MVC Application Run Successfully Done !");
+		System.out.println("Spring Security MVC Application Run Successfully Done !");	
+		System.out.println();
+		System.out.println("Default User");
+		System.out.println("-----------------------");
+		System.out.println("Username : imranmadbar");
+		System.out.println("Password : imranmadbar");
+		System.out.println("http://localhost:8181/");
+		System.out.println("Using this credential you can log in to the app as a Super admin and able to create users, assign their roles.");
+
+	
 	}
 	
 }
